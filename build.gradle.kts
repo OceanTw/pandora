5bbabc6321a865611b6e7e91c97f51ed261fb7b5
OceanTw <windgame999@gmail.com>
2025-06-02 13:20:47 -0500

    feat: spigot repository & main class setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.spigotmc:spigot-api:1.21.5-R0.1-SNAPSHOT")
 }
 
 tasks.test {
---
+++ src/main/java/dev/ocean/valblock/Plugin.java
@@ -0,0 +1,14 @@
+package dev.ocean.valblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
